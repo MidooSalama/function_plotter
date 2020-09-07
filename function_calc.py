5ded8de8232fdb6530cc5a20b89887e88078f6c3
salama <eng.mahmoud.a.salama@gmail.com>
2020-09-07 11:52:46 +0200

    calculate the expresion

--- function_calc.py
+++ function_calc.py
@@ -1,6 +1,81 @@
 import re
 
 
+def calc(operand1, operand2, operator):
+    op1 = float(operand1)
+    op2 = float(operand2)
+    if operator == '^':
+        return op1 ** op2
+    if operator == '*':
+        return op1 * op2
+    if operator == '/':
+        return op1 / op2
+    if operator == '+':
+        return op1 + op2
+    if operator == '-':
+        return op1 - op2
+
+
+def calcByIdx(fArray, idx):
+    oldFunArray = fArray.copy()
+    newFunArray = []
+    result = str(calc(oldFunArray[idx - 1], oldFunArray[idx + 1], oldFunArray[idx]))
+    for j, value in enumerate(oldFunArray):
+        if j == idx or j == idx + 1:
+            continue
+        if j == idx - 1:
+            newFunArray.append(result)
+        else:
+            newFunArray.append(value)
+
+    return newFunArray
+
+
+
+
+def calculateExpersion(functionArray, xValue):
+    found = True
+    i = -1
+    while i < len(functionArray) and found == True:
+        if 'x' in functionArray[i + 1:]:
+            i = functionArray.index('x', i + 1)
+            found = True
+            functionArray[i] = xValue
+        else:
+            found = False
+
+    # newFunction = calculateOperation(functionArray, '^')
+    newFunction = functionArray
+    # calculate ^ (from right to left)
+    while '^' in newFunction:
+        for idx,element in enumerate(newFunction[::-1]):
+            if element == '^':
+                newFunction = calcByIdx(newFunction, len(newFunction)-idx-1)
+                print(newFunction)
+                break
+    print(newFunction)
+    # calculate *, /
+    while '*' in newFunction or '/' in newFunction:
+        for idx,element in enumerate(newFunction):
+            if element == '*' or element == '/':
+                newFunction = calcByIdx(newFunction, idx)
+                print(newFunction)
+                break
+    # calculate +, -
+    while '+' in newFunction or '-' in newFunction:
+        for idx,element in enumerate(newFunction):
+            if element == '+' or element == '-':
+                newFunction = calcByIdx(newFunction, idx)
+                print(newFunction)
+                break
+    print(newFunction)
+    finalResult = newFunction[0]
+    return finalResult
+
+
+
+
+
 def function_parsing(funcString):
     negative = False
     operator = False
@@ -22,6 +97,8 @@ def function_parsing(funcString):
     functionElements.append(funcString[opIdx_old+1:index+1])
 
     print(functionElements)
+    return functionElements
+
 
 def funcStringIsValid(funcString):
     found = re.findall("[-+*/^x.0-9]",funcString) # "^-[0-9]+|^[0-9]+|[0-9]+[-+*/^][0-9]+"
@@ -59,9 +136,12 @@ fstrings = ["434",
            "5*x+6",
            "43*3+x",
            "543.53*x^24+640/33",
+           "2*8^x^1-4/2*3+70.25*x",
            "43+3.4.23*32",
            "43*34+",
            "*246"]
 for fstring in fstrings:
     if(funcStringIsValid(fstring)):
-        function_parsing(fstring)
+        fExpersionArray = function_parsing(fstring)
+        result = calculateExpersion(fExpersionArray, 2)
+        print(result)
